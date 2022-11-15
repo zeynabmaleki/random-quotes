c40593f938acc6852fa1033853db30cc60cb914f
zizimrad <73473371+zeynabmaleki@users.noreply.github.com>
2022-11-15 13:22:14 +0330

    math random

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 const quoteOptions = [{
-        "quote": "The only sin is ignorance",
+        "quote": "“The only sin is ignorance”",
         "source": "Christopher Marlowe"
     },
     {
-        "quote": "A man is his own easiest dupe, for what he wishes to be true he generally believes to be true",
+        "quote": "“A man is his own easiest dupe, for what he wishes to be true he generally believes to be true”",
         "source": "Demosthenes"
     },
     {
-        "quote": "A lie can travel halfway around the world while the truth is putting on its shoes",
+        "quote": "“A lie can travel halfway around the world while the truth is putting on its shoes”",
         "source": "Mark Twain"
     },
     {
-        "quote": "Great minds discuss ideas; average minds discuss events; small minds discuss people",
+        "quote": "“Great minds discuss ideas; average minds discuss events; small minds discuss people”",
         "source": "Eleanor Roosevelt"
     },
     {
-        "quote": "If you have a garden and a library, you have everything you need",
+        "quote": "“If you have a garden and a library, you have everything you need”",
         "source": "Marcus Tullius Cicero"
     },
     {
-        "quote": "Truth comes out in wine",
+        "quote": "“Truth comes out in wine”",
         "source": "Pliny the Elder"
     },
     {
-        "quote": "Everything in the universe is within you. Ask all from yourself",
+        "quote": "“Everything in the universe is within you. Ask all from yourself”",
         "source": "Rumi"
     },
     {
-        "quote": "When I get a little money I buy books; and if any is left I buy food and clothes",
+        "quote": "“When I get a little money I buy books; and if any is left I buy food and clothes”",
         "source": "Desiderius Erasmus"
     },
     {
@@ -75,5 +75,7 @@ const quoteOptions = [{
 document.getElementById('button').addEventListener('click', newQuote)
 
 function newQuote() {
-
-}
+    let quoteNumber = Math.floor(Math.random() * 17);
+    document.getElementById('quotes').innerText = quoteOptions[quoteNumber].quote;
+    document.getElementById('quotesource').innerText = quoteOptions[quoteNumber].source;
+};
